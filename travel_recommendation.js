2f5227d001057c5a6ffea56bba67ff7e8d2590b2
Michal Kosmulski <michal.kosmulski@allegro.com>
2025-04-28 10:02:53 -0400

    further work

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,11 +1,56 @@
+function displayRecommendation(recommendation) {
+    document.getElementById("recoName").textContent = recommendation.name;
+    document.getElementById("recoDescription").textContent = recommendation.description;
+    document.getElementById("recoImg").setAttribute("src", recommendation.imageUrl);
+}
+
 function loadRecommendation() {
     var recommendations = fetch('travel_recommendation_api.json')
         .then((response) => response.json())
         .then((json) => {
             var cities = json.countries.flatMap(country => country.cities);
             var selectedCity = cities[Math.floor(Math.random() * cities.length)];
-            document.getElementById("recoName").textContent = selectedCity.name;
-            document.getElementById("recoDescription").textContent = selectedCity.description;
-            document.getElementById("recoImg").setAttribute("src", selectedCity.imageUrl);
+            displayRecommendation(selectedCity);
         });
+}
+
+function clearSearchResults() {
+    document.getElementById("searchResults").replaceChildren();
+}
+
+function clearSearch() {
+    document.getElementById("search").textContent = "";
+    clearSearchResults();
+}
+
+function appendSearchResults(recommendations) {
+    var resultList = document.getElementById("search");
+    for (reco of recommendations) {
+        resultList.appendChild(document.createElement("p")).append(document.createElement("b")).append(recommendation.name);
+        resultList.appendChild(document.createElement("p")).append(recommendation.description);
+        var img = resultList.appendChild(document.createElement("img"));
+        img.setAttribute("style", "width: 20%");
+        img.setAttribute("src", recommendation.imageUrl);
+    }
+}
+
+function search() {
+    clearSearchResults();
+    var recommendations = fetch('travel_recommendation_api.json')
+        .then((response) => response.json());
+    var query = document.getElementById("search").textContent.toLowerCase();
+    switch(query) {
+        case "beach":
+        case "beaches":
+            appendSearchResults(recommendations.beaches);
+            break;
+        case "temple":
+        case "temples":
+            appendSearchResults(recommendations.temples);
+            break;
+        case "country":
+        case "countries":
+            appendSearchResults(recommendations.countries).flatMap(country => country.cities);
+            break;
+    }
 }
